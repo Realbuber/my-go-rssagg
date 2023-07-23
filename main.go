a11b6f3ce85ad78199e5f9bc93b232188544540c
Realbuber <ms0345313@gmail.com>
2023-07-23 15:06:34 +0800

    set up postgres in linux

--- main.go
+++ main.go
@@ -9,7 +9,14 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/go-chi/cors"
 	"github.com/go-chi/chi"
+
+	_"github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
+
 
 func main(){
 
@@ -18,6 +25,21 @@ func main(){
 	portString := os.Getenv("PORT")
 	if portString =="" {
 		log.Fatal("PORT is not found in environemnt")
+	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is not found in the environment")
+	}
+
+	conn, err := sql.Open("postgres",dbURL)
+	if err != nil{
+		log.Fatal("Can't connect to database:", err)
+	}
+
+	apiCfg := apiConfig{
+		DB:database.New(conn),
+		
 	}
 
 	router := chi.NewRouter()
@@ -34,7 +56,7 @@ func main(){
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err",handlerErr)
-
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 
 	//add a v1 prefix
@@ -47,7 +69,7 @@ func main(){
 
 	log.Printf("Server starting on port %v",portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil{
 		log.Fatal(err)
 	}
